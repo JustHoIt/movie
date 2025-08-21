54f382bf10d668035bfdfe0619f859e583e7ae5b
Just_Ho_it <zalop3128@kakao.com>
2025-08-22 06:46:32 +0900

    [FEAT] 컴포넌트 - 해쉬 라우터 관리

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import { Component } from "./core/core.js"
+import { Component } from './core/core.js'
+import TheHeader from './components/TheHeader'
 
 export default class App extends Component {
-    constructor() {
-        super({
-            state: {
-                inputText: ''
-            }
-        })
-    }
+
     render() {
-        this.el.classList.add('search')
-
-        this.el.innerHTML = /* html */ `
-        <input />
-        <button>Click</button>
-        `
-
-        const inputEl = this.el.querySelector('input')
-        inputEl.addEventListener('input', () => {
-            this.state.inputText = inputEl.value
-        })
-
-        const buttonEL = this.el.querySelector('button')
-        buttonEL.addEventListener('click', () => {
-            console.log(this.state.inputText)
-        })
+        const routerView = document.createElement('router-view')
+        this.el.append(
+            new TheHeader().el,
+            routerView
+        )
     }
 }
